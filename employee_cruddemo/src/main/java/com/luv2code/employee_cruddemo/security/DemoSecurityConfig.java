45ef2b884fe33aa0d7e901cb7536d41a69f7516b
Meet Shah <meetshah7199@gmail.com>
2023-11-23 16:04:07 -0500

    Add java code into DemoSecurityConfig for custom table.

--- employee_cruddemo/src/main/java/com/luv2code/employee_cruddemo/security/DemoSecurityConfig.java
+++ employee_cruddemo/src/main/java/com/luv2code/employee_cruddemo/security/DemoSecurityConfig.java
@@ -17,7 +17,17 @@ public class DemoSecurityConfig {
     //add support for JDBC, in normal terms I am connecting mysql database to my application, before it was manual entry
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+        //return new JdbcUserDetailsManager(dataSource);
+
+        // ----- code for query, retrieved data from custom table-----
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        // define query to retrieve a authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
